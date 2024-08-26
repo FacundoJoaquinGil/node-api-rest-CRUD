d2ab0b7a6625e4c420445d40e33fcc6d21095b87
Joaquin <FacundoJoaGL@gmail.com>
2024-08-26 19:59:33 -0300

    CRUD Finalizado

--- src/routes/booksroutes.js
+++ src/routes/booksroutes.js
@@ -26,8 +26,8 @@ const getBook = async (req, res, next) => {
         res.status(500).json({ message: error.message })
     }
 
-    //por que json?
-    res.json(book)
+    //por que esto es asi? IMPORTANTE
+    res.book = book;
     //Qué es next?
     next()
 }
@@ -71,6 +71,67 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: error.message })
     }
 })
+
+//FALTA GET INDIVIDUAL
+
+router.get('/:id', getBook, async (req, res) => {
+    res.json(res.book)
+})
+
+router.put('/:id', getBook, async (req, res) => {
+    try {
+        const book = res.book
+
+        book.title = req.body.title || book.title;
+        book.description = req.body.description || book.description;
+        book.genre = req.body.genre || book.genre;
+        book.release_Date = req.body.release_Date || book.release_Date;
+
+        const updatedBook = await book.save()
+        res.json(updatedBook)
+
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+router.patch('/:id', getBook, async (req, res) => {
+
+    if (!req.body.title && req.body.description && req.body.genre && req.body.release_Date) {
+        res.status(400).json({ message: 'Alguno de los campos deben ser modificados' })
+    }
+
+    try {
+        const book = res.book
+
+        book.title = req.body.title || book.title;
+        book.description = req.body.description || book.description;
+        book.genre = req.body.genre || book.genre;
+        book.release_Date = req.body.release_Date || book.release_Date;
+
+        const updatedBook = await book.save()
+        res.json(updatedBook)
+
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+router.delete('/:id', getBook, async (req, res) => {
+
+    try {
+        const book = res.book
+        await book.deleteOne({
+            _id: book._id
+        })
+        res.json(`El libro ${book.title} se eliminó correctamente`)
+
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+
 
 //de esta forma se exporta las rutas
 module.exports = router
